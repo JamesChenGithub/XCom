6dbe97da93c43733445c8baa69e50b8fa73e063f
alexichen <alexichen@tencent.com>
2018-10-10 22:13:07 +0800

    fix log bug

--- new/xcom/main.cpp
+++ new/xcom/main.cpp
@@ -7,9 +7,59 @@
 //
 
 #include <stdio.h>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
 #include "xcom.h"
+
+void extern_xcom_log(xcom_log_level level, const char *filename , const char *funcname, int line, const char *format, ...)
+{
+    static std::recursive_mutex logMutex;
+    std::lock_guard<std::recursive_mutex> lock(logMutex);
+    std::stringstream ss;
+    
+    va_list valist;
+    va_start(valist, format);
+    char strBuf[16 * 1024];
+    vsnprintf(strBuf, 16 * 1024, format, valist);
+    std::string str = strBuf;
+    
+    std::string lstr;
+    switch (level)
+    {
+        case xcom_log_level_info:
+            lstr = "I";
+            break;
+        case xcom_log_level_warn:
+            lstr = "W";
+            break;
+        case xcom_log_level_error:
+            lstr = "E";
+            break;
+        case xcom_log_level_debug:
+            lstr = "D";
+            break;
+        case xcom_log_level_verbose:
+            lstr = "V";
+            break;
+            
+        default:
+            break;
+    }
+    
+    ss << lstr << "|" << std::setw(15) << filename << "|" << std::setw(20) <<funcname << "|" << std::setw(5) << line <<"|" << str << std::endl ;
+    
+    std::cout << ss.str() << std::endl;
+    
+    va_end(valist);
+}
 
 int main()
 {
-    
+    xcom_log_debug("this is a test");
+    xcom_log("this is a test");
+    xcom_log("this is a test");
+    xcom_log("this is a test");
+    xcom_log("a");
+    return 0;
 }
--- new/xcom/xcom_log.h
+++ new/xcom/xcom_log.h
@@ -29,7 +29,7 @@ extern "C" {
     
 #else
     
-    extern void extern_xcom_log(xcom_log_level level, const char *filename , const char *funcname, int line, const char *tag, const char *format, ...);
+    extern void extern_xcom_log(xcom_log_level level, const char *filename , const char *funcname, int line, const char *format, ...);
     
 #if XCOM_OS_WIN
 #define __FILENAME__ (strrchr(__FILE__, '\\') + 1)
@@ -37,12 +37,12 @@ extern "C" {
 #define __FILENAME__ (strrchr(__FILE__, '/') + 1)
 #endif
     
-#define xcom_log_error(fmt, ...)    extern_xcom_log(xcom_log_level_error, __FILENAME__, __FUNCTION__, __LINE__, tag, fmt, ##__VA_ARGS__)
-#define xcom_log_warn(fmt, ...)     extern_xcom_log(xcom_log_level_warn, __FILENAME__, __FUNCTION__, __LINE__, tag, fmt, ##__VA_ARGS__)
-#define xcom_log_info(fmt, ...)     extern_xcom_log(xcom_log_level_info, __FILENAME__, __FUNCTION__, __LINE__, tag, fmt, ##__VA_ARGS__)
-#define xcom_log_debug(fmt, ...)    extern_xcom_log(xcom_log_level_debug, __FILENAME__, __FUNCTION__, __LINE__, tag, fmt, ##__VA_ARGS__)
-#define xcom_log_verbose(fmt, ...)  extern_xcom_log(xcom_log_level_verbose, __FILENAME__, __FUNCTION__, __LINE__, tag, fmt, ##__VA_ARGS__)
-#define xcom_log(fmt, ...)          xcom_log_debug(fmt, ...)
+#define xcom_log_error(fmt, ...)    extern_xcom_log(xcom_log_level_error, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define xcom_log_warn(fmt, ...)     extern_xcom_log(xcom_log_level_warn, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define xcom_log_info(fmt, ...)     extern_xcom_log(xcom_log_level_info, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define xcom_log_debug(fmt, ...)    extern_xcom_log(xcom_log_level_debug, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define xcom_log_verbose(fmt, ...)  extern_xcom_log(xcom_log_level_verbose, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define xcom_log          xcom_log_debug
     
 #endif
     
